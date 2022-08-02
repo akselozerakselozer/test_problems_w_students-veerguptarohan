c079122e9e5d08b11a6467df8c6ae91aa0571605
akselozerakselozer <110494862+akselozerakselozer@users.noreply.github.com>
2022-08-02 14:02:37 -0600

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -2,6 +2,6 @@
 
 double add_numbers(const double f1, const double f2) { return f1+f2; }
 
-double subtract_numbers(const double f1, const double f2) { return 0; }
+double subtract_numbers(const double f1, const double f2) { return f1*f2; }
 
 double multiply_numbers(const double f1, const double f2) { return 0; }
